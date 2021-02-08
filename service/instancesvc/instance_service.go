72bdf474f65a3621f5adec5dd6978fb2cac1e3c7
idealvin <yishuwen.zb@ccbft.com>
2021-02-08 16:06:21 +0800

    update instance

--- service/instancesvc/instance_service.go
+++ service/instancesvc/instance_service.go
@@ -1,8 +1,11 @@
 package instancesvc
 
 import (
+	"encoding/json"
+	"strconv"
 	"time"
 	"iaas-api-server/common"
+	"iaas-api-server/common/config"
 	"iaas-api-server/proto/instance"
 
 	log "github.com/sirupsen/logrus"
@@ -18,7 +21,7 @@ import (
 type InstanceService struct {
 }
 
-// CreateInstance 创建云主机
+/*
 func (is *InstanceService) CreateInstance(ctx context.Context, req *instance.CreateInstanceReq) (*instance.InstanceRes, error) {
 	log.Info("rpc CreateInstance req: ", req)
 	res := &instance.InstanceRes{}
@@ -52,7 +55,7 @@ func (is *InstanceService) CreateInstance(ctx context.Context, req *instance.Cre
 		Networks: []servers.Network{
 			servers.Network{UUID: req.NetworkUuid},
 		},
-		//AdminPass: "root",
+		AdminPass: req.RootPass,
 	}
 
 	// TODO: 后续在 common/error.go 中定义错误码
@@ -63,6 +66,9 @@ func (is *InstanceService) CreateInstance(ctx context.Context, req *instance.Cre
 		log.Error("openstack create instance failed: ", err)
 		return res, err
 	}
+
+	ssjson, err := json.Marshal(ss)
+	log.Info("create instance res: ", string(ssjson))
 
 	res.Code = common.EOK.Code
 	res.Msg = common.EOK.Msg
@@ -85,7 +91,139 @@ func (is *InstanceService) CreateInstance(ctx context.Context, req *instance.Cre
 		NetworkUuid:         req.NetworkUuid,
 		// TODO: SecurityGroupName:
 		InstanceName:        ss.Name,
-		HypervisorHostname:  ss.HostID,  // TODO: HostID?
+		//GuestOsHostname:  ss.HostID,  // TODO: HostID?
+		CreatedTime:         ss.Created.Local().Format("2006-01-02 03:04:05"),
+		UpdatedTime:         ss.Updated.Local().Format("2006-01-02 03:04:05"),
+	}
+
+	log.Info("rpc CreateInstance res: ", res)
+	return res, nil
+}
+*/
+
+// 创建 volume, 返回 volume id
+//   volume endpoint: http://192.168.66.131/volume/v3
+func createVolume(volumeType string, sizeInG int32, projectID string, token string) (string, error) {
+	jstmpl :=
+`{
+    "volume": {
+        "size": {{.VolumeSize}},
+        "multiattach": false,
+        "volume_type": {{.VolumeType}},
+    }
+}`
+
+	mp := map[string]string {
+		"VolumeSize": strconv.Itoa(int(sizeInG)),
+		"VolumeType": volumeType,
+	}
+
+	jsbody, _ := common.CreateJsonByTmpl(jstmpl, mp)
+
+	url, err := config.GetString("volume_endpoint")
+	if err != nil {
+		log.Error("volume endpoint not found")
+		return "", err
+	}
+
+	url = url + "/" + projectID + "/volumes"
+
+	res, err := common.CallRawAPI(url, "POST", jsbody, token)
+	if err != nil {
+		log.Error("create volume failed: ", err)
+		return "", err
+	}
+
+	rmp := make(map[string]interface{})
+	err = json.Unmarshal(res, &rmp)
+	if err != nil {
+		log.Error("json conv to map failed: ", err)
+		return "", err
+	}
+
+	log.Info("create volume res: ", string(res))
+
+}
+
+// CreateInstance 创建云主机
+func (is *InstanceService) CreateInstance(ctx context.Context, req *instance.CreateInstanceReq) (*instance.InstanceRes, error) {
+	log.Info("rpc CreateInstance req: ", req)
+	res := &instance.InstanceRes{}
+
+	token, err := common.AuthAndGetToken(req.Apikey, req.TenantId, req.PlatformUserid)
+	if err != nil {
+		log.Error("get token failed: ", err)
+		res.Code = common.EUNAUTHORED.Code
+		res.Msg = common.EUNAUTHORED.Msg
+		return res, err
+	}
+
+	provider, err := common.GetOpenstackClient(req.Apikey, req.TenantId, req.PlatformUserid)
+	if provider == nil {
+		res.Code = common.EUNAUTHORED.Code
+		res.Msg = common.EUNAUTHORED.Msg
+		log.Error("GetOpenstackClient failed: ", err)
+		return res, err
+	}
+
+	client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: req.Region,
+	})
+
+	// TODO: 后续在 common/error.go 中定义错误码
+	if err != nil {
+		res.Code = 20000
+		res.Msg = "openstack NewComputeV2 failed"
+		log.Error("openstack NewComputeV2 failed: ", err)
+		return res, err
+	}
+
+	opts := servers.CreateOpts{
+		Name:             req.InstanceName,
+		ImageRef:         req.ImageRef,
+		FlavorRef:        req.FlavorRef,
+		SecurityGroups:   req.SecurityGroupName,
+		AvailabilityZone: req.AvailabilityZone,
+		Networks: []servers.Network{
+			servers.Network{UUID: req.NetworkUuid},
+		},
+		AdminPass: req.RootPass,
+	}
+
+	// TODO: 后续在 common/error.go 中定义错误码
+	ss, err := servers.Create(client, opts).Extract()
+	if err != nil {
+		res.Code = 20001
+		res.Msg = "openstack create instance failed"
+		log.Error("openstack create instance failed: ", err)
+		return res, err
+	}
+
+	ssjson, err := json.Marshal(ss)
+	log.Info("create instance res: ", string(ssjson))
+
+	res.Code = common.EOK.Code
+	res.Msg = common.EOK.Msg
+
+	res.Instance = &instance.InstanceRes_Instance{
+		InstanceId:          ss.ID,
+		InstanceStatus:      ss.Status,
+		InstanceAddr:        ss.AccessIPv4,  // TODO: 填充 AccessIPv4?
+		Region:              req.Region,
+		AvailabilityZone:    req.AvailabilityZone,
+		Flavor:              &instance.Flavor {  // TODO: 后续根据 FlavorMap 填充
+			FlavorId: "xxx",
+			FlavorName: "xxx",
+			FlavorVcpus: "xxx",
+			FlavorRam: "xxx",
+			FlavorDisk: "xxx",
+		},
+		ImageRef:            req.ImageRef,
+		// TODO: SystemDisk:
+		NetworkUuid:         req.NetworkUuid,
+		// TODO: SecurityGroupName:
+		InstanceName:        ss.Name,
+		//GuestOsHostname:  ss.HostID,  // TODO: HostID?
 		CreatedTime:         ss.Created.Local().Format("2006-01-02 03:04:05"),
 		UpdatedTime:         ss.Updated.Local().Format("2006-01-02 03:04:05"),
 	}
@@ -125,6 +263,9 @@ func (is *InstanceService) GetInstance(ctx context.Context, req *instance.GetIns
 		return res, err
 	}
 
+	ssjson, err := json.Marshal(ss)
+	log.Info("get instance res: ", string(ssjson))
+
 	res.Code = common.EOK.Code
 	res.Msg = common.EOK.Msg
 
@@ -146,7 +287,7 @@ func (is *InstanceService) GetInstance(ctx context.Context, req *instance.GetIns
 		//NetworkUuid:         req.NetworkUuid,
 		// TODO: SecurityGroupName: ss.SecurityGroups
 		InstanceName:        ss.Name,
-		HypervisorHostname:  ss.HostID,  // TODO: HostID?
+		GuestOsHostname:     ss.HostID,
 		CreatedTime:         ss.Created.Local().Format("2006-01-02 03:04:05"),
 		UpdatedTime:         ss.Updated.Local().Format("2006-01-02 03:04:05"),
 	}
@@ -191,6 +332,14 @@ func (is *InstanceService) UpdateInstanceFlavor(ctx context.Context, req *instan
 		return res, err
 	}
 
+	err = servers.ConfirmResize(client, req.InstanceId).ExtractErr()
+	if err != nil {
+		res.Code = 20003
+		res.Msg = "openstack confirm resize instance failed"
+		log.Error("openstack confirm resize instance failed: ", err)
+		return res, err
+	}
+
 	res.Code = common.EOK.Code
 	res.Msg = common.EOK.Msg
 
@@ -206,6 +355,7 @@ func (is *InstanceService) UpdateInstanceFlavor(ctx context.Context, req *instan
 func (is *InstanceService) DeleteInstance(ctx context.Context, req *instance.DeleteInstanceReq) (*instance.DeleteInstanceRes, error) {
 	log.Info("rpc DeleteInstance req: ", req)
 	res := &instance.DeleteInstanceRes{}
+	res.InstanceId = req.InstanceId;
 
 	provider, err := common.GetOpenstackClient(req.Apikey, req.TenantId, req.PlatformUserid)
 	if provider == nil {
@@ -227,7 +377,7 @@ func (is *InstanceService) DeleteInstance(ctx context.Context, req *instance.Del
 
 	err = servers.Delete(client, req.InstanceId).ExtractErr()
 	if err != nil {
-		res.Code = 20003
+		res.Code = 20004
 		res.Msg = "openstack delete instance failed"
 		log.Error("openstack delete instance failed: ", err)
 		return res, err
@@ -235,7 +385,6 @@ func (is *InstanceService) DeleteInstance(ctx context.Context, req *instance.Del
 
 	res.Code = common.EOK.Code
 	res.Msg = common.EOK.Msg
-	res.InstanceId = req.InstanceId;
 	res.DeletedTime = time.Now().Format("2006-01-02 03:04:05")
 
 	log.Info("rpc DeleteInstance res: ", res)
@@ -285,14 +434,14 @@ func (is *InstanceService) OperateInstance(ctx context.Context, req *instance.Op
 		}
 		err = servers.Reboot(client, req.InstanceId, opts).ExtractErr()
 	} else {
-		res.Code = 20005
+		res.Code = 20006
 		res.Msg = "unsupported operate type"
 		log.Error("unsupported operate type: ", req.OperateType)
 		return res, err
 	}
 
 	if err != nil {
-		res.Code = 20004
+		res.Code = 20005
 		res.Msg = "openstack operate instance failed"
 		log.Error("openstack operate instance failed: ", err)
 		return res, err
