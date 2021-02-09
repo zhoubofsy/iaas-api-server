feb826b8d6f5d861297dfaf474e9485b933055ca
bozhou <zhoubofsy@hotmail.com>
2021-02-09 17:50:11 +0800

    增加Region选择参数

--- service/osssvc/oss_configuration.go
+++ service/osssvc/oss_configuration.go
@@ -2,7 +2,7 @@ package osssvc
 
 type OSSConfigure interface {
 	GetEndpointByRegion(string) string
-	GetRGWAdminAccessSecretKeys() (string, string)
+	GetRGWAdminAccessSecretKeys(string) (string, string)
 }
 
 type OSSSimpleConfigure struct {
@@ -12,7 +12,7 @@ func (o *OSSSimpleConfigure) GetEndpointByRegion(r string) string {
 	return "http://120.48.27.190:80"
 }
 
-func (o *OSSSimpleConfigure) GetRGWAdminAccessSecretKeys() (string, string) {
+func (o *OSSSimpleConfigure) GetRGWAdminAccessSecretKeys(r string) (string, string) {
 	return "C3ZBITE3VS5AD4Y3YEZB", "3cZZ8D7mP0hNCiqUIYnxKmhEPmbzcCFBkr7Bz4ey"
 }
 
