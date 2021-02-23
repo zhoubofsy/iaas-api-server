f120f5ba50f41e7d6361f1f2528ba7a97f6da69d
tiantingting <2565086122@qq.com>
2021-02-23 10:17:12 +0800

    OSS注释实现

--- service/osssvc/oss_configuration.go
+++ service/osssvc/oss_configuration.go
@@ -9,10 +9,13 @@ type OSSSimpleConfigure struct {
 }
 
 func (o *OSSSimpleConfigure) GetEndpointByRegion(r string) string {
+	//TODO 从配置文件中读取，r相当于Key，可能存在多个key需要比对
+
 	return "http://120.48.27.190:80"
 }
 
 func (o *OSSSimpleConfigure) GetRGWAdminAccessSecretKeys(r string) (string, string) {
+	//TODO 从数据库中获取，根据region获取AccessKey和SecretKey
 	return "C3ZBITE3VS5AD4Y3YEZB", "3cZZ8D7mP0hNCiqUIYnxKmhEPmbzcCFBkr7Bz4ey"
 }
 
