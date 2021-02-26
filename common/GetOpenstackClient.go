1753892fa8d7b6a1bd15ae621cc82fed17e6405c
mongia <mr_pengmj@outlook.com>
2021-02-26 08:37:22 +0000

    处理冲突

--- common/GetOpenstackClient.go
+++ common/GetOpenstackClient.go
@@ -1,32 +1,15 @@
 package common
 
 import (
+	"os"
+
 	sdk "github.com/gophercloud/gophercloud"
 	openstack "github.com/gophercloud/gophercloud/openstack"
-	"iaas-api-server/common/config"
 )
 
 // GetOpenstackClient is for creating an openstack provider client
 func GetOpenstackClient(apikey string, tenantID string, platformUserID string,
 	resourceID ...string) (*sdk.ProviderClient, error) {
-<<<<<<< HEAD
-	// TODO:
-	//   1. auth
-	//   2. get tenant info
-
-	identityEndpoint, _ := config.GetString("identity_endpoint")
-	username, _ := config.GetString("test_username")
-	passwd, _ := config.GetString("test_passwd")
-	prjID, _ := config.GetString("test_project_id")
-
-	opts := sdk.AuthOptions{
-		//IdentityEndpoint: "http://192.168.247.130/identity", // idenEndPoint,
-		IdentityEndpoint: identityEndpoint, // idenEndPoint,
-		Username:         username,         // username,
-		Password:         passwd,           // password,
-		DomainName:       "default",        // domain,
-		TenantID:         prjID,            // projectID,
-=======
 	authResult := APIAuth(apikey, tenantID, platformUserID, resourceID...)
 	if !authResult {
 		return nil, EAPIAUTH
@@ -43,7 +26,6 @@ func GetOpenstackClient(apikey string, tenantID string, platformUserID string,
 		Password:         tenantInfo.OpenstackPassword,   // password,
 		DomainName:       tenantInfo.OpenstackDomainname, // domain,
 		TenantID:         tenantInfo.OpenstackProjectid,  // tenantID,
->>>>>>> master
 	}
 
 	return openstack.AuthenticatedClient(opts)
