b8a662a46bf4e3f0ad0ebce09b3e8e5bb672941a
idealvin <yishuwen.zb@ccbft.com>
2021-03-15 14:12:19 +0800

    update test coding

--- client/flavor_cli.go
+++ client/flavor_cli.go
@@ -4,26 +4,37 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"iaas-api-server/common"
 	"log"
 	"time"
 
-	"iaas-api-server/proto/flavor"
 	"google.golang.org/grpc"
+	"iaas-api-server/proto/flavor"
 )
 
-func listFlavors() {
-	// Set up a connection to the server.
+var flvConn *grpc.ClientConn
+var flvCli flavor.FlavorServiceClient
+
+// Set up a connection to the server.
+func createFlavorServiceClient() (flavor.FlavorServiceClient, *grpc.ClientConn) {
+	timer := common.NewTimer()
 	conn, err := grpc.Dial(*address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("connect to %s failed: %v", *address, err)
+		return nil, nil
 	}
-	defer conn.Close()
 
 	c := flavor.NewFlavorServiceClient(conn)
+	log.Printf("connect to %s ok, time elapse: %v", *address, timer.Elapse())
+	return c, conn
+}
+
+func listFlavors() {
+	timer := common.NewTimer()
 
 	var data []byte = []byte(*param)
 	req := &flavor.ListFlavorsReq{}
-	err = json.Unmarshal(data, req)
+	err := json.Unmarshal(data, req)
 	if nil != err {
 		log.Fatalf("input param is not json or format error, err: %+v", err)
 	}
@@ -33,27 +44,20 @@ func listFlavors() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout) * time.Second)
 	defer cancel()
 
-	res, err := c.ListFlavors(ctx, req)
+	res, err := flvCli.ListFlavors(ctx, req)
 	if err != nil {
 		log.Fatalf("rpc request failed: %v", err)
 	}
 
-	log.Printf("rpc result: %+v", res)
+	log.Printf("rpc result: %+v, time elapse: %v", res, timer.Elapse())
 }
 
 func getFlavor() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("connect to %s failed: %v", *address, err)
-	}
-	defer conn.Close()
-
-	c := flavor.NewFlavorServiceClient(conn)
+	timer := common.NewTimer()
 
 	var data []byte = []byte(*param)
 	req := &flavor.GetFlavorReq{}
-	err = json.Unmarshal(data, req)
+	err := json.Unmarshal(data, req)
 	if nil != err {
 		log.Fatalf("input param is not json or format error, err: %+v", err)
 	}
@@ -63,12 +67,12 @@ func getFlavor() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout) * time.Second)
 	defer cancel()
 
-	res, err := c.GetFlavor(ctx, req)
+	res, err := flvCli.GetFlavor(ctx, req)
 	if err != nil {
 		log.Fatalf("rpc request failed: %v", err)
 	}
 
-	log.Printf("rpc result: %+v", res)
+	log.Printf("rpc result: %+v, time elapse: %v", res, timer.Elapse())
 }
 
 func main() {
@@ -78,6 +82,12 @@ func main() {
 		log.Print("usage:\n    ./cli.exe -address localhost:8080 -method GetFlavor -param '{\"xx\":\"123\"}' -timeout 1")
 		return
 	}
+
+	flvCli, flvConn = createFlavorServiceClient()
+	if flvConn == nil {
+		return;
+	}
+	defer flvConn.Close()
 
 	if *method == "ListFlavors" {
 		listFlavors()
